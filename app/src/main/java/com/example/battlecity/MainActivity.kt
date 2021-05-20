cbc272c31be04a5772fdb8fb6564c5af9c05664f
MIV228 <mivgithub@gmail.com>
2021-05-20 19:17:10 +0700

    commit

--- app/src/main/java/com/example/battlecity/MainActivity.kt
+++ app/src/main/java/com/example/battlecity/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.battlecity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+       when (keyCode) {
+           KEYCODE_DPAD_UP -> move(Direction.UP)
+           KEYCODE_DPAD_DOWN -> move(Direction.DOWN)
+           KEYCODE_DPAD_LEFT -> move(Direction.LEFT)
+           KEYCODE_DPAD_RIGHT -> move(Direction.RIGHT)
+       }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun move(direction: Direction) {
+
     }
 }
